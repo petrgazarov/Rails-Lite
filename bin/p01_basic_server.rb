dda0f7dfb45275eb08b68b4c04ffb87b3f0fd78e
Petr Gazarov <petrgazarov@gmail.com>
2015-10-22 22:18:11 -0400

    phase two

---
+++ bin/p01_basic_server.rb
@@ -0,0 +1,19 @@
+require 'webrick'
+require 'byebug'
+
+# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick.html
+# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPRequest.html
+# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPResponse.html
+# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/Cookie.html
+
+server = WEBrick::HTTPServer.new(:Port => 3000)
+
+server.mount_proc("/") do |req, res|
+
+end
+
+trap('INT') do
+  server.shutdown
+end
+
+server.start
